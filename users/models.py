60ac3b15781bb0abf677a36c82ce066220b3f108
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2022-06-02 13:41:58 +0200

    first step is done

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser): 
+    bio = models.TextField(max_length=200,blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    
+
+ 
